04bf0df248e6ed4daa13e313d619b3a0dee14444
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-12-02 13:29:26 +0700

    first commit

---
+++ main.cpp/loket.h
@@ -0,0 +1,44 @@
+#ifndef LOKET_H_INCLUDED
+#define LOKET_H_INCLUDED
+
+//adt loket
+struct loket{
+    int idLoket;
+    int harga;
+    int jumlahPenumpang;
+};
+
+typedef elmLoket *adrLoket;
+
+struct elmLoket{
+    adrLoket next;
+};
+
+//adt penumpang
+struct penumpang{
+    int idPenumpang;
+    int banyakTiket;
+};
+
+typedef struct elmPenumpang *adrPenumpang;
+
+struct elmPenumpang{
+    adrPenumpang next;
+    adrPenumpang prev;
+};
+
+//adt relasi
+typedef struct elmRelasi *adrRelasi;
+
+struct elmRelasi{
+    adrLoket nextLooket;
+    adrPenumpang nextPenumpang;
+    adrRelasi next;
+};
+
+void createLoket(listLoket &L);
+void cretaleListPenumpang(listPenumpang &L);
+void createListRelasi(listRelasi &L)
+
+
+#endif // LOKET_H_INCLUDED
