c748283de726ea25f17d56827abab00de32a7293
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-12-06 12:51:49 +0700

    second commit

--- main.cpp/loket.h
+++ main.cpp/loket.h
@@ -1,5 +1,7 @@
 #ifndef LOKET_H_INCLUDED
 #define LOKET_H_INCLUDED
+typedef int infotypeLoket;
+typedef int infotypePenumpang;
 
 //adt loket
 struct loket{
@@ -8,37 +10,64 @@ struct loket{
     int jumlahPenumpang;
 };
 
-typedef elmLoket *adrLoket;
-
-struct elmLoket{
-    adrLoket next;
-};
-
-//adt penumpang
 struct penumpang{
     int idPenumpang;
     int banyakTiket;
 };
 
+typedef struct elmLoket *adrLoket;
 typedef struct elmPenumpang *adrPenumpang;
+typedef struct elmRelasi *adrRelasi;
+
+struct elmLoket{
+    Loket info;
+    adrLoket next;
+};
 
 struct elmPenumpang{
+    Penumpang info;
     adrPenumpang next;
     adrPenumpang prev;
 };
 
-//adt relasi
-typedef struct elmRelasi *adrRelasi;
-
 struct elmRelasi{
-    adrLoket nextLooket;
+    adrLoket nextLoket;
     adrPenumpang nextPenumpang;
     adrRelasi next;
 };
 
+struct ListLoket{
+    adrLoket first;
+};
+
+struct ListPenumpang{
+    adrPenumpang first;
+    adrPenumpang last;
+};
+
+struct ListRelasi{
+    adrRelasi first;
+};
+
 void createLoket(listLoket &L);
 void cretaleListPenumpang(listPenumpang &L);
-void createListRelasi(listRelasi &L)
+void createListRelasi(listRelasi &L);
+void insertFirstLoket(ListLoket &L);
+void insertLastLoket(ListLoket &L);
+void insertFirstPenumpang(ListPenumpang &L);
+void insertLastPenumpang(ListPenumpang &L);
+void insertFirstRelasi(ListRelasi &L);
+void insertLastRelasi(ListRelasi &L);
+void delFirstLoket(ListLoket &L, adrLoket &p);
+void delLastLoket(ListLoket &L, adrLoket &p);
+void delAfterLoket(ListLoket &L, adrLoket &p, prec);
+void delFirstPenumpang(ListPenumpang &L, adrPenumpang &p);
+void delLastPenumpang(ListPenumpang &L, adrPenumpang &p);
+void delAfterPenumpang(ListPenumpang &L, adrPenumpang &p, prec);
+address findLoket(ListLoket L, infotypeLoket);
+address findPenumpang(ListPenumpang L, infotypePenumpang);
+void printInfo(List L);
+void printInfoPenumpang(ListPenumpang L);
 
 
 #endif // LOKET_H_INCLUDED
